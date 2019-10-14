af7b39e103a0e1c6e07006d5501371016d437aed
zhaopengcheng <710411486@qq.com>
2019-10-14 14:01:34 +0800

    过滤非法项目名称

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const program = require('commander');
 const pkg = require('./package');
 const path = require('path');
+const fs = require('fs-extra');
 const question = require('./src/utils/question');
 const generator = require('./src/utils/generator');
 let config = {
@@ -23,7 +24,7 @@ program
 	})
 
 	.option('-g, --package [package]', 'package manager [yarn | npm]', function(pkg) {
-		console.log('pkg', pkg)
+		console.log('pkg', pkg);
 		if (pkg && pkg !== 'yarn' && pkg !== 'npm') {
 			console.log(chalk.red('package must be "yarn" or "npm"'));
 			process.exit(-1);
@@ -37,6 +38,12 @@ async function execute(projectName) {
 	config.name = projectName;
 	if (!config.name) {
 		config.name = await question('project name?', 'input');
+	}
+	if(!/^[a-z\-_]+$/.test(config.name)) {
+		console.error(`Project names can only be composed of lowercase letters or -_`)
+		config.name = '';
+		await execute()
+		return 
 	}
 	if (!config.preprocessor) {
 		config.preprocessor = await question('css preprocessor?', 'list', {
@@ -67,11 +74,32 @@ async function execute(projectName) {
 		});
 	}
 	const targetDir = path.resolve(process.cwd(), config.name);
-	console.log('Creating a new React Component Library in', chalk.green(targetDir), '.');
-	console.log()
+	console.log('Creating a new React Component Library in ', chalk.green(targetDir), '.');
+	console.log();
 	console.log('config', chalk.green('css preprocessor'), ': ', config.preprocessor);
 	console.log('config', chalk.green('package manager'), ': ', config.pkg);
 	console.log('config', chalk.green('typescript'), ': ', config.ts ? 'enabled' : 'disabled');
-	console.log()
-	generator(config);
+	// 生成cru.config.js配置文件
+	const configPath = path.resolve(targetDir, 'cru.config.js');
+	const configContent = `module.exports = {
+  output: {
+    doc: 'build-doc',
+    library: 'build-library'
+  },
+  static: {
+    doc: 'public',
+    library: 'libraryStatic',
+  },
+  src: {
+    doc: 'doc',
+    library: 'components'
+  },
+  cssPreprocessor: ${JSON.stringify(config.preprocessor)},
+  typescript: ${JSON.stringify(config.ts)},
+  pkg: ${JSON.stringify(config.pkg)}
+}\n`;
+	await fs.writeFile(configPath, configContent);
+	console.log(`generate cru config file success: [ ${chalk.green(configPath)} ]`);
+	console.log();
+	await generator(config);
 }
