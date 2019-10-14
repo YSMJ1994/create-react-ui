86e16891b69feb4568a2f73699e059800ae16f5a
zhaopengcheng <710411486@qq.com>
2019-10-14 14:01:22 +0800

    下载后修改pkg name为项目名称

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -1,48 +1,49 @@
 const Steps = require('cli-step');
 const Spinner = require('cli-spinner').Spinner;
-const {execSync, exec, spawn} = require('child_process');
+const { execSync, exec, spawn } = require('child_process');
 const path = require('path');
-const fs = require('fs-extra')
+const fs = require('fs-extra');
 const chalk = require('chalk');
-const download = require('download-git-repo')
-const owner = 'YSMJ1994'
+const download = require('download-git-repo');
+const owner = 'YSMJ1994';
 
 function downloadFromGithub(owner, name, dest) {
 	return new Promise((resolve, reject) => {
-		download(`github:${owner}/${name}`, dest, {}, function (err) {
-			if(err) {
-				reject(err)
+		download(`github:${owner}/${name}`, dest, {}, function(err) {
+			if (err) {
+				reject(err);
 			} else {
-				resolve()
+				resolve();
 			}
-		})
-	})
+		});
+	});
 }
 
 const tempProjects = {
-	'ts': {
-		'scss': 'react-ui-temp-ts',
-		'less': ''
+	ts: {
+		scss: 'react-ui-temp-ts',
+		less: ''
 	},
-	'js': {
-		'scss': 'react-ui-temp-js',
-		'less': ''
+	js: {
+		scss: 'react-ui-temp-js',
+		less: ''
 	}
-}
+};
 
 async function generator({ name, ts, preprocessor, pkg }) {
 	const targetDir = path.resolve(process.cwd(), name);
-	if(fs.existsSync(targetDir)) {
+	if (fs.existsSync(targetDir)) {
 		console.log(`目录 ${chalk.red(name)} 已存在！`);
 		return;
 	} else {
 		fs.ensureDirSync(targetDir);
 	}
 	console.log('Installing packages. This might take a couple of minutes.');
-	const spin = new Spinner('downloading... ')
-	spin.setSpinnerString(18)
+	const spin = new Spinner('downloading... ');
+	spin.setSpinnerString(18);
 	spin.start();
 	let tempName = tempProjects[ts ? 'ts' : 'js'][preprocessor];
+
 	// 下载模板项目至目标目录
 	await downloadFromGithub(owner, tempName, targetDir);
 	spin.stop(true);
@@ -50,24 +51,29 @@ async function generator({ name, ts, preprocessor, pkg }) {
 	console.log();
 	execSync(`cd ${name} && ${pkg} install`, {
 		stdio: 'inherit'
-	})
+	});
 	console.log();
-	console.log('Initialized a git repository.')
+	console.log('Initialized a git repository.');
 	console.log();
 	execSync(`cd ${name} && git init`);
-	console.log(`Success! Created ${name} at ${targetDir}`)
-	console.log('Inside that directory, you can run several commands:')
+	// 修改pkg name
+	const targetPkgPath = path.resolve(targetDir, 'package.json');
+	const targetPkg = require(targetPkgPath);
+	targetPkg.name = name;
+	await fs.writeJSON(targetPkgPath, targetPkg, { spaces: 2 });
+	console.log(`Success! Created ${name} at ${targetDir}`);
+	console.log('Inside that directory, you can run several commands:');
 	console.log();
-	console.log(chalk.cyan(`  ${pkg} start`))
-	console.log(`    Starts the development server.`)
+	console.log(chalk.cyan(`  ${pkg} start`));
+	console.log(`    Starts the development server.`);
 	console.log();
-	console.log(chalk.cyan(`  ${pkg} build`))
-	console.log(`    Bundles the library into static files for production.`)
+	console.log(chalk.cyan(`  ${pkg} build`));
+	console.log(`    Bundles the library into static files for production.`);
 	console.log();
-	console.log(chalk.cyan(`  ${pkg} test`))
-	console.log(`    Starts the test runner.`)
+	console.log(chalk.cyan(`  ${pkg} test`));
+	console.log(`    Starts the test runner.`);
 	console.log();
-	console.log('We suggest that you begin by typing:')
+	console.log('We suggest that you begin by typing:');
 	console.log();
 	console.log(`  ${chalk.cyan('cd')} ${name}`);
 	console.log(chalk.cyan(`  ${pkg} start`));
